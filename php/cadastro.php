869ba098c0bf5206df174c14ce13c1d887ae317d
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2025-03-16 11:47:47 -0300

    Correções
    Correções

--- php/cadastro.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('conexao.php'); // Incluindo a conexão com o banco de dados
-
-if($_SERVER['REQUEST_METHOD'] == "POST"){ // Verificação se o formulário está sendo enviado
-    $nome = $_POST['nome'];
-    $email = $_POST['email']
-}
-?>
